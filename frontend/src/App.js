90d8e37d611f44f676645d61653b537c60ccc6dc
vijay <vijayagopalan190598@gmail.com>
2022-12-19 22:50:46 +0530

    Initial Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header >
+        <a href='/'>Amazon</a>
+      </header>
+      <main>
+        list products
+      </main>
+    </div>
+  );
+}
+
+export default App;
