55b0be09ffa6e31cf503e12842a2a2b8aaa0076b
vijay <vijayagopalan190598@gmail.com>
2022-12-22 13:29:56 +0530

    Page Not Found and Home Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css';
 import Home from './Home';
-import data from './data';
+import PageNotFound from './PageNotFound';
 
 
 function App() {
@@ -15,26 +15,9 @@ function App() {
       <main>
         <Routes>
           <Route path='/' excat element={<Home/>}/>
+          <Route path='*' excat element={<PageNotFound/>}/>
         </Routes>
-        <h1>Featured Products</h1>
-        <div className="products">
-          {
-            data.products.map(product => (
-              <div className="product" key={product.slug}>
-                <a href={`/product/${product.slug}`}>
-                  <img src={product.image} alt={product.name} />
-                </a>
-                <div className="product-info">
-                  <a href={`/product/${product.slug}`}>
-                    <p>{product.name}</p>
-                  </a>
-                  <p><b>${product.price}</b></p>
-                  <button>Add to cart</button>
-                </div>
-              </div>
-            ))
-          }
-        </div>
+        
       </main>
     </div>
     </BrowserRouter>
--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import data from './data';
 
 const Home = () => {
     return (
         <div>
-            Home
+            <h1>Featured Products</h1>
+        <div className="products">
+          {
+            data.products.map(product => (
+              <div className="product" key={product.slug}>
+                <a href={`/product/${product.slug}`}>
+                  <img src={product.image} alt={product.name} />
+                </a>
+                <div className="product-info">
+                  <a href={`/product/${product.slug}`}>
+                    <p>{product.name}</p>
+                  </a>
+                  <p><b>${product.price}</b></p>
+                  <button>Add to cart</button>
+                </div>
+              </div>
+            ))
+          }
+        </div>
         </div>
     );
 };
