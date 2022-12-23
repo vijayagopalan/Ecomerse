b3aa5c8ee5c120115f049c6e078a02607efd0409
vijay <vijayagopalan190598@gmail.com>
2022-12-23 13:59:48 +0530

    Fetch Sample Data Using Axios

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,31 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import data from './data';
+import axios from 'axios';
 
 const Home = () => {
-    return (
-        <div>
-            <h1>Featured Products</h1>
-        <div className="products">
-          {
-            data.products.map(product => (
-              <div className="product" key={product.slug}>
+  const [products, setProducts] = useState([]);
+  const fetchData = async () => {
+    const result = await axios.get('/api/products');
+    setProducts(result.data.products);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Featured Products</h1>
+      <div className="products">
+        {
+          products.map(product => (
+            <div className="product" key={product.slug}>
+              <Link to={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </Link>
+              <div className="product-info">
                 <Link to={`/product/${product.slug}`}>
-                  <img src={product.image} alt={product.name} />
+                  <p>{product.name}</p>
                 </Link>
-                <div className="product-info">
-                  <Link to={`/product/${product.slug}`}>
-                    <p>{product.name}</p>
-                  </Link>
-                  <p><b>${product.price}</b></p>
-                  <button>Add to cart</button>
-                </div>
+                <p><b>${product.price}</b></p>
+                <button>Add to cart</button>
               </div>
-            ))
-          }
-        </div>
-        </div>
-    );
+            </div>
+          ))
+        }
+      </div>
+    </div>
+  );
 };
 
 export default Home;
