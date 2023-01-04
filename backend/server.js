51fba49ee1980e3778a9c1eaa251de0367c81793
vijay <vijayagopalan190598@gmail.com>
2023-01-04 22:02:19 +0530

    connect to mongo DB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,15 @@
 import express from "express";
 import data from "./data.js";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
 
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.MONGODB_URI).then(()=>{
+    console.log("Connected to DB")
+}).catch((err)=>{
+    console.log(err)
+});
 const app = express();
 app.get('/api/products', (req, res) => {
     res.send(data);
@@ -24,7 +33,6 @@ app.get('/api/products/slug/:slug', (req, res) => {
     }
     res.send(data);
 });
-
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`server runs at http://localhost:${port}`);
